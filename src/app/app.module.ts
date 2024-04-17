2a3c2f86ab88d91815340255ff4bff189c69f812
Mauricio <mauriciorojasanchez@gmail.com>
2024-04-16 23:18:45 -0500

    feat: structure defined.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterContentComponent } from './public/components/footer-content/footer-content.component';
+import { ToolbarComponent } from './public/components/toolbar/toolbar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbar} from "@angular/material/toolbar";
+import { TableContentComponent } from './news/components/table-content/table-content.component';
+import {MatTable} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterContentComponent,
+    ToolbarComponent,
+    TableContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbar,
+    MatTable
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
